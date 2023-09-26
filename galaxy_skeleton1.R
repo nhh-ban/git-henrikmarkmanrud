b677b7508390239adb3470a19f0d8cd1d711bd5e
Henrik Markmanrud <henrik.markmanrud@student.nhh.no>
2023-09-26 16:57:41 +0200

    Saving variables description into txt file

--- galaxy_skeleton1.R
+++ galaxy_skeleton1.R
@@ -7,13 +7,26 @@
 # Read the entire data file into memory using the readLines()-function. Use the
 # URL direcly or read the data from the local file that is in the repository.
 
+data_file <- readLines("suites_dw_Table1.txt")
+
+
 # Identify the line number L of the separator line between the column names and
 # the rest of the data table.
+# Opening the txt file and counting rows
+
 
 # Save the variable descriptions (i.e. the information in lines 1:(L-2)) in a
 # text-file for future reference using the cat()-function
 
+top_data <- data_file[1:12]
+cat(top_data, sep = "\n", file = "top_data.txt")
+extracted_data <- data_file[13:length(data_file)]
+
+
 # Extract the variable names (i.e. line (L-1)), store the names in a vector.
+
+heading <- extracted_data[0]
+
 
 # Read the data. One way to do this is to rewrite the data to a new .csv-file
 # with comma-separators for instance using cat() again, with the variable names
