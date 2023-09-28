2606fc8c72e26a39f7b4fbbc758f7ee76e4841aa
Henrik Markmanrud <henrik.markmanrud@student.nhh.no>
2023-09-28 08:46:21 +0200

    Created clean data frame using skeleton2

--- galaxy_skeleton1.R
+++ galaxy_skeleton1.R
@@ -2,17 +2,20 @@
 # Skeleton file 1 for Assignment 1 in BAN400. 
 # -------------------------------------------
 
+library(tidyverse)
+
 # Comments below describes briefly a set of steps that solves Problem 1.
 
 # Read the entire data file into memory using the readLines()-function. Use the
 # URL direcly or read the data from the local file that is in the repository.
 
-data_file <- readLines("suites_dw_Table1.txt")
+data_file <- readLines(con = "suites_dw_Table1.txt")
 
 
-# Identify the line number L of the separator line between the column names and
-# the rest of the data table.
+# Identify the line number L (14 in this case) of the separator line between the 
+# column names and the rest of the data table.
 # Opening the txt file and counting rows
+
 
 
 # Save the variable descriptions (i.e. the information in lines 1:(L-2)) in a
@@ -25,7 +28,7 @@ extracted_data <- data_file[13:length(data_file)]
 
 # Extract the variable names (i.e. line (L-1)), store the names in a vector.
 
-heading <- extracted_data[0]
+heading <- extracted_data[1]
 
 
 # Read the data. One way to do this is to rewrite the data to a new .csv-file
